a7afa1e342a08eb7d4ba930a0ed9a2d595ff72d9
Myo Thura Ko <myothura.ko@crpusd.org>
2026-03-02 08:52:55 -0800

    finished 3.3.1 2d arrays

--- InitList2D.java
+++ InitList2D.java
@@ -10,11 +10,15 @@ public class InitList2D
                         {"band", "stand"},
                         {"out", "shine"}  }; 
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < board.length; i++)
     {
-            for (int j = 0; j < 2; j++)
-            {
-                     System.out.println(board[i][j]);
+            for (int j = 0; j < board[0].length; j++)
+            {       
+              if (i == 0 && j == 0)
+              {
+                board[i][j] = "fourth";
+              }
+              System.out.println(board[i][j]);
             }
     }
 
